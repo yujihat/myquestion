9f42a53d665989777c898914fc3b3f2ee9f042c8
yujihat <yuji.hatrock88@gmail.com>
2022-06-21 10:58:52 +0900

    Webページ完成

--- questions.js
+++ questions.js
@@ -1,25 +1,46 @@
-const bmi = calculateBmi(1.75, 69);
+const answersList = document.querySelectorAll('ol.answers li');
 
-let message = 'わかりません';
+answersList.forEach(li => li.addEventListener('click', checkClickedAnswer));
 
-if (bmi < 18.5){
-	//痩せ
-	message = '低体重です';
-}else if (bmi >= 25.0){
-	//肥満
-	message = '太っています';
-}else {
-	message = '普通です';
+//正しい答え
+const correctAnswers = {
+	question1:'D',
+	question2:'A',
+	question3:'D',
+	question4:'C',
+};
+
+function checkClickedAnswer(event){
+	//クリックされた答えの要素(liタグ)
+	const clickedAnswerElement = event.currentTarget;
+	//console.log(clickedAnswerElement.closest('ol.answers').dataset.id);
+//選択した答え(A,B,C,D)
+const selectedAnswer = clickedAnswerElement.dataset.answer;
+
+const questionId = clickedAnswerElement.closest('ol.answers').dataset.id;
+
+//正しい答え(A,B,C,D)
+const correctAnswer = correctAnswers[questionId];
+//メッセージをれる変数を用意
+let message;
+//カラーコードを入れる変数を用意
+let answerColorCode;
+
+if(selectedAnswer == correctAnswer){
+//正しい答えだったとき
+message = '正解です！おめでとう';
+answerColorCode = '';
+}else{
+//間違いだったとき
+message = '残念、不正解^^;';
+answerColorCode = '#f05959';
 }
 
-console.log(message);
 
-function calculateBmi(height,weight){
-
-	//BMI = 体重[kg] / (身長[m] ** 2)
-	return weight /(height ** 2);
-
+	alert(message);
+	//色を変更(間違っていた時だけ色が変わる)
+	document.querySelector('span#correct-answer').style.color = answerColorCode;
+	//答え全体を表示
+	document.querySelector('div#section-correct-answer').style.display ='block';
 }
 
-//BMI値が18.5未満か判定する
-//25以上奈良肥満
